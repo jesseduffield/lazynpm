a9d00e3b55535cbc6cf57a7008ab8bf430addce0
Jesse Duffield <jessedduffield@gmail.com>
2020-04-08 19:05:14 +1000

    WIP

--- pkg/gui/packages_panel.go
+++ pkg/gui/packages_panel.go
@@ -14,95 +14,45 @@ import (
 
 // list panel functions
 
-func (gui *Gui) getSelectedFile() (*commands.File, error) {
-	selectedLine := gui.State.Panels.Files.SelectedLine
-	if selectedLine == -1 {
-		return &commands.File{}, gui.Errors.ErrNoFiles
+func (gui *Gui) getSelectedPackage() *commands.File {
+	if len(gui.State.Packages) == 0 {
+		return nil
 	}
-
-	return gui.State.Files[selectedLine], nil
+	return gui.State.Packages[gui.State.Panels.Packages.SelectedLine]
 }
 
-func (gui *Gui) selectFile(alreadySelected bool) error {
-	gui.getFilesView().FocusPoint(0, gui.State.Panels.Files.SelectedLine)
+func (gui *Gui) selectPackage() error {
+	gui.getPackagesView().FocusPoint(0, gui.State.Panels.Packages.SelectedLine)
 
-	if gui.inDiffMode() {
-		return gui.renderDiff()
-	}
-
-	file, err := gui.getSelectedFile()
-	if err != nil {
-		if err != gui.Errors.ErrNoFiles {
-			return err
-		}
-		gui.State.SplitMainPanel = false
+	pkg := gui.getSelectedPackage()
+	if pkg == nil {
 		gui.getMainView().Title = ""
-		return gui.newStringTask("main", gui.Tr.SLocalize("NoChangedFiles"))
+		return gui.newStringTask("main", gui.Tr.SLocalize("NoChangedPackages"))
 	}
 
-	if !alreadySelected {
-		if err := gui.resetOrigin(gui.getMainView()); err != nil {
-			return err
-		}
-		if err := gui.resetOrigin(gui.getSecondaryView()); err != nil {
-			return err
-		}
+	if err := gui.resetOrigin(gui.getMainView()); err != nil {
+		return err
 	}
-
-	if file.HasInlineMergeConflicts {
-		gui.getMainView().Title = gui.Tr.SLocalize("MergeConflictsTitle")
-		gui.State.SplitMainPanel = false
-		return gui.refreshMergePanel()
-	}
-
-	if file.HasStagedChanges && file.HasUnstagedChanges {
-		gui.State.SplitMainPanel = true
-		gui.getMainView().Title = gui.Tr.SLocalize("UnstagedChanges")
-		gui.getSecondaryView().Title = gui.Tr.SLocalize("StagedChanges")
-		cmdStr := gui.GitCommand.DiffCmdStr(file, false, true)
-		cmd := gui.OSCommand.ExecutableFromString(cmdStr)
-		if err := gui.newPtyTask("secondary", cmd); err != nil {
-			return err
-		}
-	} else {
-		gui.State.SplitMainPanel = false
-		if file.HasUnstagedChanges {
-			gui.getMainView().Title = gui.Tr.SLocalize("UnstagedChanges")
-		} else {
-			gui.getMainView().Title = gui.Tr.SLocalize("StagedChanges")
-		}
-	}
-
-	cmdStr := gui.GitCommand.DiffCmdStr(file, false, !file.HasUnstagedChanges && file.HasStagedChanges)
-	cmd := gui.OSCommand.ExecutableFromString(cmdStr)
-	if err := gui.newPtyTask("main", cmd); err != nil {
+	if err := gui.resetOrigin(gui.getSecondaryView()); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (gui *Gui) refreshFiles() error {
-	selectedFile, _ := gui.getSelectedFile()
-
-	filesView := gui.getFilesView()
-	if filesView == nil {
+func (gui *Gui) refreshPackages() error {
+	packagesView := gui.getPackagesView()
+	if packagesView == nil {
 		// if the filesView hasn't been instantiated yet we just return
 		return nil
 	}
-	if err := gui.refreshStateFiles(); err != nil {
+	if err := gui.refreshStatePackages(); err != nil {
 		return err
 	}
 
 	gui.g.Update(func(g *gocui.Gui) error {
-		displayStrings := presentation.GetFileListDisplayStrings(gui.State.Files, gui.State.Diff.Ref)
-		gui.renderDisplayStrings(filesView, displayStrings)
-
-		if g.CurrentView() == filesView || (g.CurrentView() == gui.getMainView() && g.CurrentView().Context == "merging") {
-			newSelectedFile, _ := gui.getSelectedFile()
-			alreadySelected := newSelectedFile.Name == selectedFile.Name
-			return gui.selectFile(alreadySelected)
-		}
+		displayStrings := presentation.GetPackageListDisplayStrings(gui.State.Packages)
+		gui.renderDisplayStrings(packagesView, displayStrings)
 		return nil
 	})
 
@@ -119,20 +69,15 @@ func (gui *Gui) PrepareSubProcess(g *gocui.Gui, commands ...string) {
 	})
 }
 
-func (gui *Gui) handleRefreshFiles(g *gocui.Gui, v *gocui.View) error {
-	return gui.refreshSidePanels(refreshOptions{scope: []int{FILES}})
-}
-
-func (gui *Gui) refreshStateFiles() error {
+func (gui *Gui) refreshStatePackages() error {
 	// get files to stage
-	files := gui.GitCommand.GetStatusFiles()
-	gui.State.Files = gui.GitCommand.MergeStatusFiles(gui.State.Files, files)
+	gui.State.Packages = gui.GitCommand.GetStatusPackages()
 
-	gui.refreshSelectedLine(&gui.State.Panels.Files.SelectedLine, len(gui.State.Files))
+	gui.refreshSelectedLine(&gui.State.Panels.Packages.SelectedLine, len(gui.State.Packages))
 	return nil
 }
 
-func (gui *Gui) onFilesPanelSearchSelect(selectedLine int) error {
-	gui.State.Panels.Files.SelectedLine = selectedLine
-	return gui.focusAndSelectFile(gui.g, gui.getFilesView())
+func (gui *Gui) onPackagesPanelSearchSelect(selectedLine int) error {
+	gui.State.Panels.Packages.SelectedLine = selectedLine
+	return gui.focusAndSelectFile(gui.g, gui.getPackagesView())
 }
